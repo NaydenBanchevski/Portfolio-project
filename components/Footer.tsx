c970014dd7bc7c0416309cc71a7a89e500d743cb
Nayden Banchevski <163778420+NaydenBanchevski@users.noreply.github.com>
2024-06-17 18:18:56 -0500

    Footer Fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
               className=" md:mt-0 mt-5 w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 hover:scale-[120%]  transition-all duration-300 "
               key={profile.id}
             >
-              <img src={profile.img} alt={profile.id} width={20} height={20} />
+              <img src={profile.img} alt="icons" width={20} height={20} />
             </div>
           ))}
         </div>
